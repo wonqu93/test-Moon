afbb6ca4df538d515a2532beb26fde14fe31a427
WonGyu Moon <mwk2470@gmail.com>
2022-03-23 20:49:07 +0900

    first

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("안녕! C#!");
+            Console.WriteLine("C# 안녕!");
+
+            var num1 = 1;
+            var num2 = 2;
+
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
