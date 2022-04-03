e48586f7aabff6e9bc12e8cf8ed71bdff25bfd28
WonGyu Moon <mwk2470@gmail.com>
2022-04-03 22:53:18 +0900

    윈폼

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -10,6 +10,22 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            var num = 1;
+
+            List<int> list = new List<int>();
+            list.Add(1); 
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+            list.Add(8);
+
+            foreach (var numm in list)
+            {
+                Console.WriteLine(numm);
+            }
         }
     }
 }
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+
+            saveFile.Title = "텍스트파일 저장해보리기";
+
+            saveFile.DefaultExt = "txt";
+
+            saveFile.Filter = "Text|*.txt";
+
+
+
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFile.FileName, textBox1.Text);
+            }
+        }
+    }
+}
